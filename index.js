cf885601933026a5a160fc2e5a295b16ec3918a9
JvQ <jhersvinvillodasquinto@gmail.com>
2022-06-16 21:57:08 -0500

    Cambios a barra nav, logo y pagina de inicio

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ var loader =document.getElementById("preloader");
 window.addEventListener("load",function(){
     loader.style.display="none"
 })
+
+const header = document.querySelector("header");
+const sectionOneOptions={rootMargin:"-200px 0px 0px 0px"};
+const selectionOne= document.querySelector(".home-intro");
+
+const sectionOneObserver= new IntersectionObserver(function(entries, sectionOneObserver){
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            header.classList.add("nav-scrolled");
+        }
+        else{
+            header.classList.remove("nav-scrolled");
+        }
+
+    })
+},sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
